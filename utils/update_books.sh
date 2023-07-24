3cbd73c91d46b0c5119d5de06a054299d76290d8
erees1 <edward.r.rees@zoho.com>
2023-07-23 22:33:31 -0700

    update books

--- utils/update_books.sh
+++ utils/update_books.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 
 web_books_path=_pages/books.md
-obsidian_books_path=~/git/md-notes/personal/Books.md
+obsidian_books_path="$HOME/git/md-notes/3 Personal Projects/Books.md"
 
 # Extract front matter from current file
 while read line; do
@@ -14,8 +14,8 @@ while read line; do
 done < $web_books_path > $web_books_path.tmp
 
 # Ignore asterixed bullets and stuff after | 
-tail -n +5 $obsidian_books_path | head -n 3 >> $web_books_path.tmp
-tail -n +5 $obsidian_books_path | awk -F '|' '{print $2}' \
+tail -n +5 "$obsidian_books_path" | head -n 3 >> $web_books_path.tmp
+tail -n +5 "$obsidian_books_path" | awk -F '|' '{print $2}' \
   | awk '{$1=$1};1' | grep -v '^-*$'| grep -v '.*\*$' \
   | grep -v '^Book$' | sed 's/^/- /g' >> $web_books_path.tmp 
 mv $web_books_path.tmp $web_books_path
